471d6264b1a5ff52ad2d0e09617bc0ff4c12bb3c
Jaakko <jiisala@gmail.com>
2023-01-09 14:25:50 +0200

    Board is created and has a block

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,3 +1,5 @@
+import { Block } from "./Block.mjs";
+
 export class Board {
   width;
   height;
@@ -5,9 +7,24 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.currentBlock = new Block(".")
   }
-
+  drop(block){
+    this.currentBlock = block
+  }
   toString() {
-    return "TODO";
+    let board = ""
+    for (let j = 0; j < this.width; j++){
+      for (let k = 0; k < this.height; k++){
+        if (j === 0 && k === 1){
+          board += this.currentBlock.color
+        }
+        else{
+          board += "."
+        }
+      }
+      board += "\n"
+    }
+    return board;
   }
 }
