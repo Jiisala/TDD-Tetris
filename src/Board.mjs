a717066fc0724b9bc7e8de20aa1220a2290d323a
Jaakko <jiisala@gmail.com>
2023-01-09 14:36:04 +0200

    Block moves down

--- src/Board.mjs
+++ src/Board.mjs
@@ -8,15 +8,19 @@ export class Board {
     this.width = width;
     this.height = height;
     this.currentBlock = new Block(".")
+    this.currentTick = 0
   }
   drop(block){
     this.currentBlock = block
+  }
+  tick(){
+    this.currentTick += 1
   }
   toString() {
     let board = ""
     for (let j = 0; j < this.width; j++){
       for (let k = 0; k < this.height; k++){
-        if (j === 0 && k === 1){
+        if (j === this.currentTick && k === 1){
           board += this.currentBlock.color
         }
         else{
